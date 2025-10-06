6cb5398fc899165d8fe96a8d507a9d1fbbd62b94
gui10l1 <guiribeiro2003@gmail.com>
2025-10-06 18:14:19 -0300

    add: atom container

--- src/modules/farmers/ui/templates/FarmersHomeTemplate/index.tsx
+++ src/modules/farmers/ui/templates/FarmersHomeTemplate/index.tsx
@@ -1,7 +1,7 @@
+import Container from "../../../../shared/ui/atoms/Container";
 import Header from "../../../../shared/ui/organisms/Header";
 import FarmersList from "../../organisms/FarmersList";
 import type { IFarmersHomeTemplateProps } from "./interfaces";
-import { Container } from "./styles";
 
 export default function FarmersHomeTemplate({ farmers }: IFarmersHomeTemplateProps) {
   return (
