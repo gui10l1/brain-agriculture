02b851634ec25fbd1a8e2e3ac3f21fadc1ffeab6
gui10l1 <guiribeiro2003@gmail.com>
2025-10-07 16:54:39 -0300

    style: form and container atom

--- src/modules/shared/ui/atoms/Container/styles.ts
+++ src/modules/shared/ui/atoms/Container/styles.ts
@@ -2,4 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   padding: 20px;
+
+  max-width: 70%;
+
+  margin: 0 auto;
 `;
--- src/modules/shared/ui/atoms/Form/styles.ts
+++ src/modules/shared/ui/atoms/Form/styles.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
 export const Container = styled.form`
-  max-width: 70%;
   margin: 0 auto;
 `;
