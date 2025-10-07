fb8415274d8a9e1d873452c5612caa396a820cfd
gui10l1 <guiribeiro2003@gmail.com>
2025-10-07 16:56:22 -0300

    change: rewrite form to fit semantic

--- src/modules/farmers/ui/organisms/FarmerForm/index.tsx
+++ src/modules/farmers/ui/organisms/FarmerForm/index.tsx
@@ -2,45 +2,56 @@ import Row from "../../../../shared/ui/atoms/Row";
 import InputGroup from "../../../../shared/ui/molecules/InputGroup";
 import { Controller, useForm } from 'react-hook-form';
 import type { IFormFarmerData } from "./interfaces";
+import Card from "../../../../shared/ui/molecules/Card";
+import FormAtom from "../../../../shared/ui/atoms/Form";
+import Button from "../../../../shared/ui/atoms/Button";
 
 export default function Form() {
   const { control } = useForm<IFormFarmerData>();
 
   return (
-    <Row>
-      <Controller
-        name="name"
-        control={control}
-        render={({ field }) => (
-          <InputGroup
-            label="Nome"
-            input={{
-              type: 'text',
-              placeholder: 'Nome do agricultor',
-              name: field.name,
-              onChange: field.onChange,
-              value: field.value,
-            }}
+    <Card title="Agricultor">
+      <FormAtom>
+        <Row>
+          <Controller
+            name="name"
+            control={control}
+            render={({ field }) => (
+              <InputGroup
+                label="Nome"
+                input={{
+                  type: 'text',
+                  placeholder: 'Nome do agricultor',
+                  name: field.name,
+                  onChange: field.onChange,
+                  value: field.value,
+                }}
+              />
+            )}
           />
-        )}
-      />
 
-      <Controller
-        name="document"
-        control={control}
-        render={({ field }) => (
-          <InputGroup
-            label="Documento"
-            input={{
-              type: 'text',
-              placeholder: 'Nome do agricultor',
-              name: field.name,
-              onChange: field.onChange,
-              value: field.value,
-            }}
+          <Controller
+            name="document"
+            control={control}
+            render={({ field }) => (
+              <InputGroup
+                label="Documento"
+                input={{
+                  type: 'text',
+                  placeholder: 'Nome do agricultor',
+                  name: field.name,
+                  onChange: field.onChange,
+                  value: field.value,
+                }}
+              />
+            )}
           />
-        )}
-      />
-    </Row>
+        </Row>
+
+        <Row align="right">
+          <Button>Continuar</Button>
+        </Row>
+      </FormAtom>
+    </Card>
   );
 }
--- src/modules/farmers/ui/templates/FarmersCreateTemplate/index.tsx
+++ src/modules/farmers/ui/templates/FarmersCreateTemplate/index.tsx
@@ -1,18 +1,10 @@
 import Container from "../../../../shared/ui/atoms/Container";
-import Header from "../../../../shared/ui/organisms/Header";
 import FarmerForm from "../../organisms/FarmerForm";
-import { Form } from "./styles";
 
 export default function FarmersCreateTemplate() {
   return (
-    <>
-      <Header showBackButton />
-
-      <Container>
-        <Form>
-          <FarmerForm />
-        </Form>
-      </Container>
-    </>
+    <Container>
+      <FarmerForm />
+    </Container>
   );
 }
